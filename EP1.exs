6757c4187e8f39a4f9d21a08c557694a5770f361
paula-ueda <kekedia@gmail.com>
2018-02-10 18:56:28 -0200

    Update EP1.exs

--- EP1.exs
+++ EP1.exs
@@ -1,6 +1,6 @@
 
 defmodule Utils do
-	def belongs?(par, conjunto) do
+	def belongs(par, conjunto) do
 		Enum.member?(conjunto, par)
 	end
 	def getx(par) do
@@ -12,9 +12,15 @@ defmodule Utils do
 	def union(a, b) do
 		Enum.concat(a, b) |> Enum.uniq
 	end
+
 	def reflexive_closure(lista_pares, lista_elementos) do
 		lista_pares_reflexivos = Recor_reflexive.reflexive(lista_elementos, [])
 		Utils.union(lista_pares, lista_pares_reflexivos)
+	end
+
+	def transitive_closure(lista_pares) do
+		Transitive.loop1(lista_pares, [])
+
 	end
 
 end
@@ -31,13 +37,40 @@ defmodule Recor_reflexive do
 	end
 end
 
+defmodule Transitive do
+	def loop1(lista_pares, lista_pares_transitivos) when length (lista_pares) <= 0 do
+		lista_pares_transitivos
+	end 
+	def loop1(lista_pares, lista_pares_transitivos) do:
+		lista_pares_transitivos = union(lista_pares_transitivos, lista_pares)
+		[first|rest] = lista_pares
+		x1 = Utils.getx(first)
+		y1 = Utils.gety(first)
+		lista_pares_transitivos = Transitive.loop2(lista_pares_transitivos, [], x1, y1)
+		lista_pares_transitivos = Transitive.loop1(rest, lista_pares_transitivos)
+	end
+	def loop2 (lista_pares_iterando, lista_pares_transitivos, x1, y1) when length (lista_pares) <= 0 do
+		lista_pares_transitivos
+	end
+	def loop2 (lista_pares_iterando, lista_pares_transitivos, x1, y1) do
+		lista_pares_transitivos = union (lista_pares_transitivos, lista_pares_iterando)
+		[first|rest] = lista_pares_iterando
+		x2 = Utils.getx(first)
+		y2 = Utils.gety(first)
+		
+		condicao = (x2==y1) && (Utils.belongs([x1, y2], lista_pares_transitivos) == false)
+		if condicao? do
+			lista_pares_transitivos = Utils.union (lista_pares_transitivos, [[x1, y2]])
+			lista_pares_transitivos = transitive_closure(lista_pares_transitivos)
+		end
+		lista_pares_transitivos = Transitive.loop2(rest, lista_pares_transitivos, x1, y1)
+	end
+end
+
 
 lista_elementos = [1,2,3]
-lista_pares = [[1,5], [1,6], [2,1], [2,2], [3,5]]
-resp = Utils.reflexive_closure(lista_pares, lista_elementos)
+lista_pares = [[1,2], [2,3], [2,1], [3,2]]
+resp = Utils.transitive_closure(lista_pares)
 IO.inspect resp, label: "The hope is"
-par = [2,5]
-x = Utils.getx(par)
-IO.puts x
 
 
